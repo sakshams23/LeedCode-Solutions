39f71b47b18270715dd2615d9bbe87fc982a057d
Saksham Soni <110776016+sakshams23@users.noreply.github.com>
2023-02-08 00:53:57 +0530

    Time: 1 ms (99.22%), Space: 40.1 MB (83.29%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -0,0 +1,21 @@
+class Solution {
+    public boolean isValid(String s) {
+         Stack<Character> st = new Stack<>();
+         for (char c : s.toCharArray()) {
+         if(c=='(' || c=='[' || c=='{'){
+             st.push(c);
+         }
+         else if (c == ')' && !st.isEmpty() && st.peek() == '(') {
+                st.pop();
+            } else if (c == '}' && !st.isEmpty() && st.peek() == '{') {
+                st.pop();
+            } else if (c == ']' && !st.isEmpty() && st.peek() == '[') {
+                st.pop();
+            }
+            else {
+                return false;
+            }
+         }
+            return st.isEmpty();  
+    }
+}
